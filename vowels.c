21e6ad50b21e116763e4f58e1af8c68371252392
mathushri <46916374+mathushri@users.noreply.github.com>
2019-01-29 12:35:38 +0530

    Update and rename even.c to vowels.c

---
+++ vowels.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+printf("enter the alphabets":\a);
+scanf("%d",&a);
+if(the alphabet a,e,i,o,u)
+printf("the alphabets are vowels");
+else
+if(except the alphabet a,e,i,o,u)
+printf("the alphabets are consonants)
+getch();
+       }  
+  
+
