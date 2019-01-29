1ab890cc773d1a56ec426b29978b5db47c3ce98b
mathushri <46916374+mathushri@users.noreply.github.com>
2019-01-29 12:44:55 +0530

    Update and rename vowels.c to alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+printf("enter the alphabets":\a);
+scanf("%d",&a);
+if(the letters from a to z)
+printf("the letters are a alphabet");
+else
+not
+getch();
+}
+      
+
+  
+
