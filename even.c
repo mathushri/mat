78272a8a9aa34afe443ab90006d722b07d3254d3
mathushri <46916374+mathushri@users.noreply.github.com>
2019-01-29 12:26:28 +0530

    Update and rename README.md to even.c

---
+++ even.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+printf("enter the number":\n);
+scanf("%d",&n);
+if(the number is divisible by 2");
+printf("the number is even");
+else
+if("the number is not divisible by 2");
+printf("the number is odd");
+getch();
+}
